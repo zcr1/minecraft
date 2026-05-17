1b95fc40d06a95cd754dad569a586961da034c2d
Zach Reinhardt <zach@novacredit.com>
2026-05-17 13:32:29 -0500

    add sass plugin

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSass } from "@rsbuild/plugin-sass";
 import path from "path";
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-    plugins: [pluginReact()],
+    plugins: [pluginReact(), pluginSass()],
     resolve: {
         alias: {
             engine: path.resolve(__dirname, "src/engine"),
